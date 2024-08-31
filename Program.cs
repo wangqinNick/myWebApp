297fd575a441c54122a724732b1210786c8c92a8
wangqinNick <347656409nick@gmail.com>
2024-08-31 16:22:14 +0800

    Feature 1 added to Program.cs

--- Program.cs
+++ Program.cs
@@ -3,6 +3,14 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+// Add session services
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -17,6 +25,17 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+
+// Add custom middleware to log requests and responses
+app.Use(async (context, next) =>
+{
+    Console.WriteLine($"Incoming Request: {context.Request.Method} {context.Request.Path}");
+    await next.Invoke();
+    Console.WriteLine($"Outgoing Response: {context.Response.StatusCode}");
+});
+
+// Add session middleware
+app.UseSession();
 
 app.UseAuthorization();
 
